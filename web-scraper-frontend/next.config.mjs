747418d6afae1c5db832471504c385ebe8566d63
Kayne Khoury <kaynekhoury1@gmail.com>
2024-08-07 23:15:29 -0500

    render config

--- web-scraper-frontend/next.config.mjs
+++ web-scraper-frontend/next.config.mjs
@@ -4,7 +4,10 @@ export default {
     async rewrites() {
         return [{
             source: '/api/:path*',
-            destination: process.env.API_URL || 'http://localhost:8000/api/:path*', // Proxy to FastAPI backend
+            destination: `${process.env.API_URL}/api/:path*` || 'http://localhost:8000/api/:path*', // Proxy to FastAPI backend
         }, ];
+    },
+    env: {
+        API_URL: process.env.API_URL, // Ensure API_URL is available on the client
     },
 };
