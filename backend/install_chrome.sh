ef43a7f0dcfb40eb4c1856652844dda5bc5100c0
Kayne Khoury <kaynekhoury1@gmail.com>
2024-08-07 23:37:34 -0500

    install chrome driver

---
+++ backend/install_chrome.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Download and install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin/
+rm chromedriver_linux64.zip
+
+# Make ChromeDriver executable
+chmod +x /usr/local/bin/chromedriver
+
+# Verify installation
+google-chrome --version
+chromedriver --version
