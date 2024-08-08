a6b9a22a39d98369e6abb8708d3823dfe553ad26
Kayne Khoury <kaynekhoury1@gmail.com>
2024-08-07 23:41:17 -0500

    add writable path

--- backend/install_chrome.sh
+++ backend/install_chrome.sh
@@ -5,15 +5,22 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get update
 sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install ChromeDriver
-CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+# Install ChromeDriver in a writable directory
+CHROMEDRIVER_DIR="/opt/chromedriver"
+
+if [ -z "$CHROMEDRIVER_VERSION" ]; then
+  CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+else
+  CHROME_DRIVER_VERSION=$CHROMEDRIVER_VERSION
+fi
+
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /usr/local/bin/
+unzip chromedriver_linux64.zip -d $CHROMEDRIVER_DIR
 rm chromedriver_linux64.zip
 
 # Make ChromeDriver executable
-chmod +x /usr/local/bin/chromedriver
+chmod +x $CHROMEDRIVER_DIR/chromedriver
 
 # Verify installation
 google-chrome --version
-chromedriver --version
+$CHROMEDRIVER_DIR/chromedriver --version
